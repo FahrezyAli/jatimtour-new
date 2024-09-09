033b11fc97d7630971ce6d45603ec9f8f65c091d
Ali Ahmad Fahrezy <47922563+FahrezyAli@users.noreply.github.com>
2024-09-09 07:22:07 +0700

    Delete tailwind directory

--- tailwind/lib/extensions/TwContextExtension.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-/// Extension Methods for context
-extension ContextExtension on BuildContext {
-  // Get screen size
-  Size get size => MediaQuery.of(this).size;
-  double get width => MediaQuery.of(this).size.width;
-  double get height => MediaQuery.of(this).size.height;
-}
